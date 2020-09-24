c9e3c11e75b5454aef257242f78f0838f107c287
Ryan Barth <ryan@barth.tech>
2020-09-24 02:51:44 -0700

    version: 0.3.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="concourse-github-status",
-    version='0.3.0'
+    version='0.3.1',
     packages=find_packages(),
     entry_points={
         "console_scripts": [
